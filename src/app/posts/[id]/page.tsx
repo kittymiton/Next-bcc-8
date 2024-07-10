79e756268309f3abad0e70891968eaa00e43a33f
kittymiton <kitty.miton@gmail.com>
2024-07-10 18:03:30 +0900

    feat:課題9の実装（microCMS）

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { API_BASE_URL } from "@/_constants/constants";
-import type { Post } from "@/_types/Post";
+import type { MicroCmsPost } from "@/_types/MicroCmsPost";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Page() {
   const { id } = useParams();
-  const [post, setPost] = useState<Post | null>(null);
+  const [post, setPost] = useState<MicroCmsPost | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<Error | null>(null);
 
@@ -15,13 +15,18 @@ export default function Page() {
     const fetcher = async (): Promise<void> => {
       try {
         setLoading(true);
-        const res = await fetch(`${API_BASE_URL}/posts/${id}`);
+        const res = await fetch(`${API_BASE_URL}/posts/${id}`, {
+          headers: {
+            // fetch関数の第二引数にheadersを設定、APIキー設定
+            "X-MICROCMS-API-KEY": process.env.NEXT_PUBLIC_MICROCMS_API_KEY as string,
+          },
+        });
         if (!res.ok) {
           throw new Error("データが見つかりません");
         }
-        const { post } = (await res.json()) as { post: Post };
-        console.log(post);
-        setPost(post);
+        const data = await res.json();
+        //console.log(data);
+        setPost(data); // dataをそのままセット
       } catch (error) {
         if (error instanceof Error) {
           setError(error);
@@ -48,12 +53,12 @@ export default function Page() {
   return (
     <main>
       <div className="post-detail">
-        <img src={post.thumbnailUrl} alt={post.title} />
+        <img src={post.thumbnail.url} alt={post.title} height={300} />
         <div className="post-info">
           <p>{new Date(post.createdAt).toLocaleDateString()}</p>
           <ul>
             {post.categories?.map((category) => (
-              <li key={category}>{category}</li>
+              <li key={category.id}>{category.name}</li>
             ))}
           </ul>
         </div>
